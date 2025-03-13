4aea6e845a059d5b85718b38f2d7078159a1f53d
mm5622 <mm5622@tux5.cci.drexel.edu>
2025-03-13 13:14:53 -0400

    adding files

--- 6-RShell/dshlib.h
+++ 6-RShell/dshlib.h
@@ -3,49 +3,48 @@
 
 #include <stdbool.h>
 
-// Constants for command structure sizes
 #define EXE_MAX 64
 #define ARG_MAX 256
 #define CMD_MAX 8
 #define CMD_ARGV_MAX (CMD_MAX + 1)
-#define N_ARG_MAX 15  // MAX number of args for a command
+#define N_ARG_MAX 15  
 
-// Longest command that can be read from the shell
+
 #define SH_CMD_MAX (EXE_MAX + ARG_MAX)
 
-// File permissions for redirection
+
 #define FILE_PERMISSIONS 0644
 
-// Special character #defines
+
 #define SPACE_CHAR  ' '
 #define PIPE_CHAR   '|'
 #define PIPE_STRING "|"
 
-// Shell prompt and exit command
+
 #define SH_PROMPT "dsh4> "
 #define EXIT_CMD "exit"
 
-// Exit status codes
+
 #define RC_SC           99
 #define EXIT_SC         100
 
-// Standard Return Codes
+
 #define OK                       0
 #define WARN_NO_CMDS            -1
 #define ERR_TOO_MANY_COMMANDS   -2
 #define ERR_CMD_OR_ARGS_TOO_BIG -3
-#define ERR_CMD_ARGS_BAD        -4  // for extra credit
+#define ERR_CMD_ARGS_BAD        -4  
 #define ERR_MEMORY              -5
 #define ERR_EXEC_CMD            -6
 #define OK_EXIT                 -7
 
-// Output constants
+
 #define CMD_OK_HEADER       "PARSED COMMAND LINE - TOTAL COMMANDS %d\n"
 #define CMD_WARN_NO_CMD     "warning: no commands provided\n"
 #define CMD_ERR_PIPE_LIMIT  "error: piping limited to %d commands\n"
 #define BI_NOT_IMPLEMENTED  "not implemented"
 
-// Dragon ASCII art
+
 #define DRAGON "\
                                                                         @%%%%                       \n\
                                                                      %%%%%%                         \n\
@@ -86,7 +85,6 @@
                      %%%%%%%%%%%%%%%%%%% %%%%%% %%                          %%%%%%%%%%%%%@          \n\
                                                                                  %%%%%%%@           \n"
 
-// Command buffer structure
 typedef struct cmd_buff {
     int  argc;
     char *argv[CMD_ARGV_MAX];
@@ -96,24 +94,24 @@ typedef struct cmd_buff {
     int outputAppend;
 } cmd_buff_t;
 
-// Command list structure
+
 typedef struct command_list {
     int num;
     cmd_buff_t commands[CMD_MAX];
 } command_list_t;
 
-// Built-in command enumeration
+
 typedef enum {
     BI_CMD_EXIT,
     BI_CMD_DRAGON,
     BI_CMD_CD,
-    BI_CMD_RC,              // extra credit command
-    BI_CMD_STOP_SVR,        // new command "stop-server"
+    BI_CMD_RC,              
+    BI_CMD_STOP_SVR,        
     BI_NOT_BI,
     BI_EXECUTED,
 } Built_In_Cmds;
 
-// Prototypes
+
 int alloc_cmd_buff(cmd_buff_t *cmd_buff);
 int free_cmd_buff(cmd_buff_t *cmd_buff);
 int clear_cmd_buff(cmd_buff_t *cmd_buff);
@@ -125,16 +123,15 @@ int handleInputRedirection(cmd_buff_t *cmd);
 int handleOutputRedirection(cmd_buff_t *cmd);
 void executeProcess(cmd_buff_t *cmd);
 
-// Built-in command functions
 Built_In_Cmds match_command(const char *input);
 Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd);
 
-// Main execution context
+
 int exec_local_cmd_loop();
 int exec_cmd(cmd_buff_t *cmd);
 int execute_pipeline(command_list_t *clist);
 
-// Extra credit: Dragon command
+
 void print_dragon();
 
-#endif // __DSHLIB_H__
+#endif 
