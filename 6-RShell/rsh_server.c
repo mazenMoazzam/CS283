0e7931a88535eeb7d81812f6c37798d96b57bc7d
mm5622 <mm5622@tux5.cci.drexel.edu>
2025-03-18 13:48:56 -0400

    Adding files

--- 6-RShell/rsh_server.c
+++ 6-RShell/rsh_server.c
@@ -11,28 +11,6 @@
 #include "dshlib.h"
 #include "rshlib.h"
 
-/*
- * start_server(ifaces, port, is_threaded)
- *      ifaces:  a string in ip address format, indicating the interface
- *              where the server will bind.  In almost all cases it will
- *              be the default "0.0.0.0" which binds to all interfaces.
- *              note the constant RDSH_DEF_SVR_INTFACE in rshlib.h
- * 
- *      port:   The port the server will use.  Note the constant 
- *              RDSH_DEF_PORT which is 1234 in rshlib.h.  If you are using
- *              tux you may need to change this to your own default, or even
- *              better use the command line override -s implemented in dsh_cli.c
- *              For example ./dsh -s 0.0.0.0:5678 where 5678 is the new port  
- * 
- *      is_threaded:  Used for extra credit to indicate the server should implement
- *                   per thread connections for clients  
- * 
- *      This function basically runs the server by: 
- *          1. Booting up the server
- *          2. Processing client requests until the client requests the
- *             server to stop by running the `stop-server` command
- *          3. Stopping the server. 
- */
 int start_server(char *ifaces, int port, int is_threaded) {
     int serverSocket;
     int returnCode;
@@ -45,32 +23,15 @@ int start_server(char *ifaces, int port, int is_threaded) {
         return errorCode;
     }
 
-
     returnCode = process_cli_requests(serverSocket);
     stop_server(serverSocket);
     return returnCode;
 }
 
-/*
- * stop_server(svr_socket)
- *      svr_socket: The socket that was created in the boot_server()
- *                  function. 
- * 
- *      This function simply returns the value of close() when closing
- *      the socket.  
- */
 int stop_server(int svr_socket) {
     return close(svr_socket);
 }
 
-/*
- * boot_server(ifaces, port)
- *      ifaces & port:  see start_server for description.  They are passed
- *                      as is to this function.   
- * 
- *      This function "boots" the rsh server.  It is responsible for all
- *      socket operations prior to accepting client connections.  
- */
 int boot_server(char *ifaces, int port) {
     int serverSocket;
     int returnStatusCode;
@@ -80,7 +41,6 @@ int boot_server(char *ifaces, int port) {
         return ERR_RDSH_COMMUNICATION;
     }
 
-   
     int enable = 1;
     if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
         close(serverSocket);
@@ -111,21 +71,15 @@ int boot_server(char *ifaces, int port) {
     return serverSocket;
 }
 
-/*
- * process_cli_requests(svr_socket)
- *      svr_socket:  The server socket that was obtained from boot_server()
- *   
- *  This function handles managing client connections.
- */
 int process_cli_requests(int svr_socket) {
     int clientSocket;
-    struct sockaddr_in cli_addr;
-    socklen_t cli_len = sizeof(cli_addr);
+    struct sockaddr_in cliAddr;
+    socklen_t cliLen = sizeof(cliAddr);
     int statusReturnCode = OK;
 
     while (1) {
 
-        clientSocket = accept(svr_socket, (struct sockaddr *)&cli_addr, &cli_len);
+	clientSocket = accept(svr_socket, (struct sockaddr *)&cliAddr, &cliLen);
         if (clientSocket < 0) {
             return ERR_RDSH_COMMUNICATION;
         }
@@ -140,24 +94,24 @@ int process_cli_requests(int svr_socket) {
     return statusReturnCode;
 }
 
-/*
- * exec_client_requests(cli_socket)
- *      cli_socket:  The server-side socket that is connected to the client
- *   
- *  This function handles accepting remote client commands.
- */
+
+
+
+
+
 int exec_client_requests(int cli_socket) {
     int ioSize;
-    command_list_t cmd_list;
+    command_list_t cmdList;
     int returnCode;
     char *ioBuffSize;
     ioBuffSize = malloc(RDSH_COMM_BUFF_SZ);
+    
     if (!ioBuffSize) {
         return ERR_RDSH_SERVER;
     }
 
     while (1) {
-        ioSize = recv(cli_socket, ioBuffSize, RDSH_COMM_BUFF_SZ, 0);
+	ioSize = recv(cli_socket, ioBuffSize, RDSH_COMM_BUFF_SZ, 0);
         if (ioSize < 0) {
             free(ioBuffSize);
             return ERR_RDSH_COMMUNICATION;
@@ -166,36 +120,30 @@ int exec_client_requests(int cli_socket) {
             return OK;
         }
         ioBuffSize[ioSize] = '\0';
-        returnCode = build_cmd_list(ioBuffSize, &cmd_list);
+        returnCode = build_cmd_list(ioBuffSize, &cmdList);
         if (returnCode < 0) {
             send_message_string(cli_socket, CMD_ERR_RDSH_EXEC);
             send_message_eof(cli_socket);
             continue;
         }
 
-        
-        Built_In_Cmds bi_cmd = rsh_built_in_cmd(&cmd_list.commands[0]);
-        if (bi_cmd == BI_CMD_EXIT) {
+        Built_In_Cmds biCmd = rsh_built_in_cmd(&cmdList.commands[0]);
+        if (biCmd == BI_CMD_EXIT) {
             free(ioBuffSize);
             return OK;
-        } else if (bi_cmd == BI_CMD_STOP_SVR) {
-            
+        } else if (biCmd == BI_CMD_STOP_SVR) {
             free(ioBuffSize);
             return OK_EXIT;
-        } else if (bi_cmd == BI_EXECUTED) {
-          
+        } else if (biCmd == BI_EXECUTED) {
             send_message_eof(cli_socket);
             continue;
         }
 
-        
-        returnCode = rsh_execute_pipeline(cli_socket, &cmd_list);
+        returnCode = rsh_execute_pipeline(cli_socket, &cmdList);
         if (returnCode < 0) {
-           
             send_message_string(cli_socket, CMD_ERR_RDSH_EXEC);
         }
 
-        
         returnCode = send_message_eof(cli_socket);
         if (returnCode != OK) {
             printf(CMD_ERR_RDSH_COMM);
@@ -209,19 +157,13 @@ int exec_client_requests(int cli_socket) {
     return OK;
 }
 
-/*
- * rsh_execute_pipeline(int cli_sock, command_list_t *clist)
- *      cli_sock:    The server-side socket that is connected to the client
- *      clist:       The command_list_t structure that we implemented in
- *                   the last shell. 
- *   
- *  This function executes the command pipeline.
- */
 int rsh_execute_pipeline(int cli_sock, command_list_t *clist) {
     int pipes[clist->num - 1][2];  
     pid_t pids[clist->num];
-    int pids_st[clist->num];       
-    int exit_code;
+    int pidsSt[clist->num];       
+    int exitCode;
+
+
     for (int i = 0; i < clist->num - 1; i++) {
         if (pipe(pipes[i]) == -1) {
             return ERR_RDSH_COMMUNICATION;
@@ -234,14 +176,12 @@ int rsh_execute_pipeline(int cli_sock, command_list_t *clist) {
             return ERR_RDSH_COMMUNICATION;
         } else if (pids[i] == 0) {
             if (i > 0) {
-                
                 dup2(pipes[i - 1][0], STDIN_FILENO);
             } else {
                 dup2(cli_sock, STDIN_FILENO);
             }
 
             if (i < clist->num - 1) {
-                
                 dup2(pipes[i][1], STDOUT_FILENO);
             } else {
                 if (!clist->commands[i].outputFile) {
@@ -250,13 +190,11 @@ int rsh_execute_pipeline(int cli_sock, command_list_t *clist) {
                 }
             }
 
-            
             for (int j = 0; j < clist->num - 1; j++) {
                 close(pipes[j][0]);
                 close(pipes[j][1]);
             }
 
-        
             execvp(clist->commands[i].argv[0], clist->commands[i].argv);
             exit(EXIT_FAILURE);
         }
@@ -267,81 +205,51 @@ int rsh_execute_pipeline(int cli_sock, command_list_t *clist) {
         close(pipes[i][1]);
     }
 
-   
     for (int i = 0; i < clist->num; i++) {
-        waitpid(pids[i], &pids_st[i], 0);
-   
+        waitpid(pids[i], &pidsSt[i], 0);
     }
-    exit_code = WEXITSTATUS(pids_st[clist->num - 1]);
-    return exit_code;
+    exitCode = WEXITSTATUS(pidsSt[clist->num - 1]);
+    return exitCode;
 }
 
-/*
- * send_message_eof(cli_socket)
- *      cli_socket:  The server-side socket that is connected to the client
- * 
- *  Sends the EOF character to the client.
- */
 int send_message_eof(int cli_socket) {
-    int send_len = (int)sizeof(RDSH_EOF_CHAR);
-    int sent_len;
-    sent_len = send(cli_socket, &RDSH_EOF_CHAR, send_len, 0);
+    int sendLen = (int)sizeof(RDSH_EOF_CHAR);
+    int sentLen;
+    sentLen = send(cli_socket, &RDSH_EOF_CHAR, sendLen, 0);
 
-    if (sent_len != send_len) {
+    if (sentLen != sendLen) {
         return ERR_RDSH_COMMUNICATION;
     }
     return OK;
 }
 
-/*
- * send_message_string(cli_socket, char *buff)
- *      cli_socket:  The server-side socket that is connected to the client
- *      buff:        A C string (aka null terminated) of a message we want
- *                   to send to the client. 
- *   
- *  Sends a message to the client.
- */
 int send_message_string(int cli_socket, char *buff) {
-    int sent_len;
-    sent_len = send(cli_socket, buff, strlen(buff), 0);
-    if (sent_len < 0) {
+    int sentLen;
+    sentLen = send(cli_socket, buff, strlen(buff), 0);
+    if (sentLen < 0) {
         return ERR_RDSH_COMMUNICATION;
     }
     return send_message_eof(cli_socket);
 }
 
-/*
- * rsh_built_in_cmd(cmd_buff_t *cmd)
- *      cmd:  The cmd_buff_t of the command, remember, this is the 
- *            parsed version of the command
- *   
- *  This function checks if the command is built-in or not. If it is,
- *  it handles the execution of the built-in command.
- */
-Built_In_Cmds rsh_built_in_cmd(cmd_buff_t *cmd)
-{
+Built_In_Cmds rsh_built_in_cmd(cmd_buff_t *cmd) {
     Built_In_Cmds ctype = BI_NOT_BI;
     ctype = rsh_match_command(cmd->argv[0]);
 
-    switch (ctype)
-    {
-    // case BI_CMD_DRAGON:
-    //     print_dragon();
-    //     return BI_EXECUTED;
-    case BI_CMD_EXIT:
-        return BI_CMD_EXIT;
-    case BI_CMD_STOP_SVR:
-        return BI_CMD_STOP_SVR;
-    case BI_CMD_RC:
-        return BI_CMD_RC;
-    case BI_CMD_CD:
-        chdir(cmd->argv[1]);
-        return BI_EXECUTED;
-    default:
-        return BI_NOT_BI;
+    switch (ctype) {
+        case BI_CMD_EXIT:
+            return BI_CMD_EXIT;
+        case BI_CMD_STOP_SVR:
+            return BI_CMD_STOP_SVR;
+        case BI_CMD_RC:
+            return BI_CMD_RC;
+        case BI_CMD_CD:
+            chdir(cmd->argv[1]);
+            return BI_EXECUTED;
+        default:
+            return BI_NOT_BI;
     }
 }
-
 
 Built_In_Cmds rsh_match_command(const char *cmd) {
     if (cmd == NULL) {
